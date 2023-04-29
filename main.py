313dad4db9b65ae110c5a3547329bb344b498cfa
Nazar Rudenok <122445773+nazarrudenok@users.noreply.github.com>
2023-04-29 12:33:15 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import fake_useragent
 import requests
 import time
 from bs4 import BeautifulSoup
-# from RussianLosses import *
+from RussianLosses import *
 from config import TOKEN
 
 user = fake_useragent.UserAgent().random
@@ -18,17 +18,12 @@ r = requests.get(url)
 
 bs = BeautifulSoup(r.text, 'html.parser')
 
-def personnel(item):
-    if item == 'total':
-        total = bs.find('div', class_ = 'loses-item-title').find_all('span')[0].text
-        return total
-
-# def date_func():
-#     url = 'https://www.minusrus.com/'
-#     r = requests.get(url, headers=headers_)
-#     bs = BeautifulSoup(r.text, 'html.parser')
-#     date_ = bs.find('div', class_ = 'date').find('span').text
-#     return date_
+def date_func():
+    url = 'https://www.minusrus.com/'
+    r = requests.get(url, headers=headers_)
+    bs = BeautifulSoup(r.text, 'html.parser')
+    date_ = bs.find('div', class_ = 'date').find('span').text
+    return date_
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -68,7 +63,7 @@ def callback(call):
             planes_en = types.KeyboardButton('Planes')
             helicopters_en = types.KeyboardButton('Helicopters')
             artillery_en = types.KeyboardButton('Artillery')
-            warships_en = types.KeyboardButton('Ships')
+            warships_en = types.KeyboardButton('Warships')
             
             markup_reply.add(personnel_en, tanks_en, bbm_en, planes_en, helicopters_en, artillery_en, warships_en)
 
@@ -81,5 +76,33 @@ def text(message):
     mch = message.chat.id
     mt = message.text
     if mt == 'Особовий склад':
-        bot.send_message(mch, personnel('total'))
+        bot.send_message(mch, f'Станом на {date_func()}\nЗнищено особового складу: {personnel("total")}\nЗа останню добу: {personnel("quantity")}')
+    elif mt == 'Танки':
+        bot.send_message(mch, f'Станом на {date_func()}\nЗнищено танків: {tanks("total")}\nЗа останню добу: {tanks("quantity")}')
+    elif mt == 'ББМ':
+        bot.send_message(mch, f'Станом на {date_func()}\nЗнищено броньованих машин: {bbm("total")}\nЗа останню добу: {bbm("quantity")}')
+    elif mt == 'Літаки':
+        bot.send_message(mch, f'Станом на {date_func()}\nЗнищено літаків: {planes("total")}\nЗа останню добу: {planes("quantity")}')
+    elif mt == 'Гелікоптери':
+        bot.send_message(mch, f'Станом на {date_func()}\nЗнищено гелікоптерів: {helicopters("total")}\nЗа останню добу: {helicopters("quantity")}')
+    elif mt == 'Артилерія':
+        bot.send_message(mch, f'Станом на {date_func()}\nЗнищено артилерійських установок: {artillery("total")}\nЗа останню добу: {artillery("quantity")}')
+    elif mt == 'Кораблі':
+        bot.send_message(mch, f'Станом на {date_func()}\nЗнищено кораблів: {warships("total")}\nЗа останню добу: {warships("quantity")}')
+    
+    if mt == 'Personnel':
+        bot.send_message(mch, f'As of {date_func()}\nPersonnel were destroyed: {personnel("total")}\nFor the last day: {personnel("quantity")}')
+    elif mt == 'Tanks':
+        bot.send_message(mch, f'As of {date_func()}\nTanks were destroyed: {tanks("total")}\nFor the last day: {tanks("quantity")}')
+    elif mt == 'Armored vehicle':
+        bot.send_message(mch, f'As of {date_func()}\nArmored vehicles were destroyed: {bbm("total")}\nFor the last day: {bbm("quantity")}')
+    elif mt == 'Planes':
+        bot.send_message(mch, f'As of {date_func()}\nPlanes were destroyed: {planes("total")}\nFor the last day: {planes("quantity")}')
+    elif mt == 'Helicopters':
+        bot.send_message(mch, f'As of {date_func()}\nHelicopters were destroyed: {helicopters("total")}\nFor the last day: {helicopters("quantity")}')
+    elif mt == 'Artillery':
+        bot.send_message(mch, f'As of {date_func()}\nArtillery were destroyed: {artillery("total")}\nFor the last day: {artillery("quantity")}')
+    elif mt == 'Warships':
+        bot.send_message(mch, f'As of {date_func()}\nWarships were destroyed: {warships("total")}\nFor the last day: {warships("quantity")}')
+
 bot.polling()
